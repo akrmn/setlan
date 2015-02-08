7f7269339aa5feaec2d014ce1a88bfa6f495eba8
akrmn <unacson@gmail.com>
2015-02-08 18:36:16 -0430

    removed Token import

--- AST.hs
+++ AST.hs
@@ -1,7 +1,5 @@
 module AST where
 
-import Tokens
-
 data Program = Program Inst deriving (Eq, Show)
 
 data Exp = Plus      Exp    Exp
@@ -31,20 +29,57 @@ data Exp = Plus      Exp    Exp
          | Or        Exp    Exp
          | Not       Exp
          | Negative  Exp
-         | Number    Token
-         | Var       Token
+         | Number    String
+         | Var       String
          | Set       [Exp]
-         | BoolTrue
-         | BoolFalse
-         | Strng Token
+         | Boolean   Bool
+         | Strng     String
          deriving (Eq, Show)
 
-data Inst = Assign  Token  Exp
+tabs :: Int -> String
+tabs = concat . flip replicate "  "
+
+-- show' :: Exp -> String
+-- show' Plus      Exp    Exp
+-- show' Minus     Exp    Exp
+-- show' Times     Exp    Exp
+-- show' Div       Exp    Exp
+-- show' Mod       Exp    Exp
+-- show' SetUnion  Exp    Exp
+-- show' SetMinus  Exp    Exp
+-- show' SetInter  Exp    Exp
+-- show' SetMax    Exp
+-- show' SetMin    Exp
+-- show' SetSize   Exp
+-- show' MapPlus   Exp    Exp
+-- show' MapMinus  Exp    Exp
+-- show' MapTimes  Exp    Exp
+-- show' MapDiv    Exp    Exp
+-- show' MapMod    Exp    Exp
+-- show' CompLT    Exp    Exp
+-- show' CompLE    Exp    Exp
+-- show' CompGT    Exp    Exp
+-- show' CompGE    Exp    Exp
+-- show' CompEQ    Exp    Exp
+-- show' CompNE    Exp    Exp
+-- show' CompAt    Exp    Exp
+-- show' And       Exp    Exp
+-- show' Or        Exp    Exp
+-- show' Not       Exp
+-- show' Negative  Exp
+-- show' Number    Token
+-- show' Var       Token
+-- show' Set       [Exp]
+-- show' BoolTrue
+-- show' BoolFalse
+-- show' Strng Token
+
+data Inst = Assign  String  Exp
           | Block   {
                       using :: Maybe [Declare],
                       instructions :: [Inst]
                     }
-          | Scan    Token
+          | Scan    String
           | Print   [Exp]
           | If      {
                       condition :: Exp,
@@ -55,7 +90,7 @@ data Inst = Assign  Token  Exp
           | WhileDo Exp    Inst
           | Repeat  Inst   Exp
           | For     {
-                      variable :: Token,
+                      variable :: String,
                       direction :: Direction,
                       set :: Exp,
                       instruction :: Inst
@@ -66,6 +101,6 @@ data Direction = Min | Max deriving (Eq, Show)
 
 data Using  = Using [Declare] deriving (Eq, Show)
 
-data Declare = Declare Type [Token] deriving (Eq, Show)
+data Declare = Declare Type [String] deriving (Eq, Show)
 
 data Type = BoolType | IntType | SetType deriving (Eq, Show)
